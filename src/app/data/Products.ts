c8f349d7b31083c3ae188471ffe8ba5c875c2b78
Juliano Grams <julianograms@gmail.com>
2024-04-06 01:38:17 -0300

    Added _id to the products source

--- src/app/data/Products.ts
+++ src/app/data/Products.ts
@@ -1,16 +1,17 @@
 import productList from "./products.json";
 import { IProduct } from "../types/IProduct";
 
-const formatProduct = (product: any) => {
+const formatProduct = (product: any, index: number): IProduct => {
   return {
     ...product,
+    _id: index,
     price: parseFloat(product.price),
   };
 };
 
 const [heros, products] = productList.reduce(
-  (acc: [IProduct[], IProduct[]], _product) => {
-    const product: IProduct = formatProduct(_product);
+  (acc: [IProduct[], IProduct[]], _product, i: number) => {
+    const product: IProduct = formatProduct(_product, i);
     if (product.hero) {
       acc[0].push(product);
     } else {
--- src/app/types/IProduct.ts
+++ src/app/types/IProduct.ts
@@ -1,4 +1,5 @@
 export interface IProduct {
+  _id: number;
   name: string;
   detail: string;
   price: number;
